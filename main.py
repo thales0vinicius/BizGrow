219924d5428beabb1de03abecece1b083f0ac5d5
Mirageblex <161901272+Mirageblex@users.noreply.github.com>
2024-08-08 22:05:29 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 from flask import Flask, render_template, redirect
-#import mysql.connector
 
 app = Flask(__name__)
 
-# conexaoDB = mysql.connector.connect(
-#     host="localhost",
-#     user="root",
-#     password="",
-#     database=""
-# )
-
+# TELA HOME
 @app.route('/')
 def home():
-    # comandoSQL = 'SELECT * FROM Vagas'
-    # cursorDB = conexaoDB.cursor()
-    # cursorDB.execute(comandoSQL)
-    # vagas = cursorDB.fetchall()
-    # cursorDB.close()
-    # return render_template("home.html", vagas=vagas)
     return render_template("home.html")
 
+# DADOS BANCÁRIOS DO USUÁRIO
+@app.route('/banco')
+def banco():
+    return render_template("banco.html")
+
+# CONTA DO USUÁRIO(LOGIN/CADASTRO)
+@app.route('/cadastro')
+def cadastro():
+    return render_template("criar_conta.html")
+
+@app.route('/login')
+def login():
+    return render_template("acessar_conta.html")
+
+# CRIAÇÃO DO PROJETO 
+@app.route('/cria_projeto')
+def criacao_projeto():
+    return render_template("criação_do_projeto_1.html")
+
+@app.route('/cria_projeto2')
+def criacao_projeto2():
+    return render_template("criação_do_projeto_2.html")
+
+@app.route('/perfil')
+def perfil():
+    return render_template("perfil.html")
+
+#APP RUN (RODA A APLICAÇÃO )
 app.run(host='0.0.0.0',port=5000, debug=True)
